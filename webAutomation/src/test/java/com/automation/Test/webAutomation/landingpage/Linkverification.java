949ded733640158bbc35d8d569194c5f346c4a6f
bilarn <bilarn@apples-mbp-2261>
2018-05-03 00:03:10 +0100

    links

--- webAutomation/src/test/java/com/automation/Test/webAutomation/landingpage/Linkverification.java
+++ webAutomation/src/test/java/com/automation/Test/webAutomation/landingpage/Linkverification.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
